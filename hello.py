39389533c6db02eb1128f7a818fb79b3ea5b331d
Michael Pradel <michael@binaervarianz.de>
2023-09-20 11:29:26 +0200

    Suppression moves to line 9; old warning moves to line 10. New, accidentally suppressed warning introduces at line 12.

--- hello.py
+++ hello.py
@@ -4,6 +4,9 @@ Toy example.
 
 def hello():
     """ A fantastic docstring,
-        which even spans two lines. """
+        which even spans two lines.
+    """
     # pylint: disable=missing-format-string-key
     print("Hello %(first)s %(last)s" % {"First": "John", "last": "Doe"})
+    print("\n")
+    print("Hello %(first)s %(last)s" % {"first_name": "John", "last": "Doe"})
